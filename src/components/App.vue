6534a4319e30558142d93c32078dcf951b708c66
Brent Nishioka <brentnishioka@gmail.com>
2023-06-08 20:41:52 -0700

    cleared vue project

--- src/components/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <router-view />
-</template>
-
-<script setup>
-import { RouterView } from "vue-router";
-</script>
-
-<style scoped></style>
--- src/pages/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <font-awesome-icon icon="fa-solid fa-home"></font-awesome-icon>
-  <h1>Home</h1>
-</template>
-
-<script></script>
-
-<style></style>
